1b76348e1f21b000c9aeaed24af82d646dba64df
1halfplusminus <dartuchiwa@gmail.com>
2021-05-15 18:30:09 +1100

    Fix storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,8 @@
 module.exports = {
   stories: [],
   addons: ['@storybook/addon-essentials'],
+  babel: async (options) => ({
+    ...options,
+    plugins: ['@babel/plugin-proposal-class-properties'],
+  }),
 };
