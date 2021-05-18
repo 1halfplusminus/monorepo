26f1568ebb199f36c573f82fa7e5cdb93bb6a016
1halfplusminus <dartuchiwa@gmail.com>
2021-05-18 14:16:06 +1100

    Fix responsive ipad pro

--- libs/ui/src/covid/stats.tsx
+++ libs/ui/src/covid/stats.tsx
@@ -9,7 +9,8 @@ export interface StatsProps {}
 const StyledStats = styled.div`
   ${tw`px-2 py-2 gap-x-1 gap-y-2 grid-rows-2 grid-cols-2`}
   ${tw`md:grid-cols-2 md:grid-flow-row md:px-5 md:gap-x-5`}
-  ${tw`lg:px-12 lg:py-10 lg:gap-x-24 lg:gap-y-4 lg:grid-rows-4 lg:grid-cols-3 `}
+  ${tw`lg:px-12 lg:py-10 lg:gap-x-5 lg:gap-y-4 lg:grid-rows-3 lg:grid-cols-3 `}
+  ${tw`xl:gap-x-28`}
   ${tw`grid grid-flow-row w-full bg-gray-200`}
 `;
 
