b826ceb71f1979e9e48d9d7cd68a84a0cf7aae2e
1halfplusminus <dartuchiwa@gmail.com>
2021-09-13 19:19:25 +1100

    use best trade get result from quoter

--- libs/redcross-swap-contract/libs/hooks/useBestTrade.ts
+++ libs/redcross-swap-contract/libs/hooks/useBestTrade.ts
@@ -1,7 +1,19 @@
 import { SupportedChainId } from '../constants/chains';
 import type { Option } from 'fp-ts/Option';
-import { option as O, array as A } from 'fp-ts';
-import { Currency, CurrencyAmount } from '@uniswap/sdk-core';
+import {
+  option as O,
+  array as A,
+  function as F,
+  taskOption as TO,
+} from 'fp-ts';
+import { CurrencyAmount, Token } from '@uniswap/sdk-core';
+import { IQuoter } from '../../typechain/IQuoter';
+import { useAllRoute } from './useAllRoute';
+import { encodeRouteToPath, Pool, Route } from '@uniswap/v3-sdk';
+import { BigNumberish } from 'ethers';
+import { useMemo } from 'react';
+import { pipe } from 'fp-ts/function';
+import { sequenceT } from 'fp-ts/lib/Apply';
 
 const QUOTE_GAS_OVERRIDES: { [chainId: number]: number } = {
   [SupportedChainId.OPTIMISM]: 6_000_000,
@@ -10,13 +22,84 @@ const QUOTE_GAS_OVERRIDES: { [chainId: number]: number } = {
 
 const DEFAULT_GAS_QUOTE = 2_000_000;
 interface UseBestV3TradeExactIn {
-  amountIn: Option<CurrencyAmount<Currency>>;
-  currencyOut: Option<CurrencyAmount<Currency>>;
+  tokenIn: Option<Token>;
+  tokenOut: Option<Token>;
   chainId: Option<number>;
+  quoter: Option<IQuoter>;
+  pools: Option<Pool[]>;
+  amountIn: Option<BigNumberish>;
 }
 
 export const useBestV3TradeExactIn = ({
-  amountIn,
-  currencyOut,
+  tokenIn,
+  tokenOut,
   chainId,
-}: UseBestV3TradeExactIn) => {};
+  quoter,
+  pools,
+  amountIn,
+}: UseBestV3TradeExactIn) => {
+  const routes = useAllRoute({ chainId, tokenIn, tokenOut, pools });
+  const currencyAmount = useMemo(
+    () =>
+      pipe(
+        sequenceT(O.Apply)(amountIn, tokenIn),
+        O.map(([amountIn, tokenIn]) =>
+          CurrencyAmount.fromRawAmount(tokenIn, amountIn.toString())
+        )
+      ),
+    [amountIn, tokenIn]
+  );
+  const quoteExactInInputs = useMemo(
+    () =>
+      pipe(
+        sequenceT(O.Apply)(routes, currencyAmount),
+        O.map(([routes, amountIn]) =>
+          pipe(
+            routes,
+            A.map((route) => [
+              encodeRouteToPath(route, false),
+              `Ox${amountIn.quotient.toString(16)}`,
+            ])
+          )
+        )
+      ),
+    [currencyAmount, routes]
+  );
+  const quotesResults = useMemo(
+    () =>
+      pipe(
+        sequenceT(O.Apply)(quoter, quoteExactInInputs, currencyAmount),
+        O.map(([quoter, quoteExactInInputs, currencyAmount]) =>
+          pipe(
+            quoteExactInInputs,
+            A.map(([path, amountIn]) =>
+              TO.tryCatch(() => quoter.quoteExactInput(path, amountIn))
+            ),
+            TO.sequenceArray
+          )
+        ),
+        TO.fromOption,
+        TO.flatten
+      ),
+    [quoter, quoteExactInInputs, currencyAmount]
+  );
+
+  return useMemo(
+    () =>
+      pipe(
+        quotesResults,
+        TO.map((results) =>
+          pipe(
+            [...results],
+            A.reduce(
+              {} as { bestRoute: Route<Token, Token>; amountOut: BigNumberish },
+              (acc) => {
+                return acc;
+              }
+            )
+          )
+        )
+      ),
+    [amountIn, tokenOut, quotesResults, routes]
+  );
+};
